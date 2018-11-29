17117259dad9f1e5b38f25c4f68c0aa2862308ac
Peggy Li <xpeggyxli@gmail.com>
2018-11-28 19:49:25 -0500

    Create new backend and update frontent

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,128 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Pusher from 'pusher-js';
 import './App.css';
-import NavBar from './components';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userMessage: '',
+      conversation: [],
+    };
+  }
+
+  componentDidMount() {
+    const pusher = new Pusher('<your app key>', {
+      cluster: '<your app cluster>',
+      encrypted: true,
+    });
+
+    const channel = pusher.subscribe('bot');
+    channel.bind('bot-response', data => {
+      const msg = {
+        text: data.message,
+        user: 'ai',
+      };
+      this.setState({
+        conversation: [...this.state.conversation, msg],
+      });
+    });
+  }
+
+  handleChange = event => {
+    this.setState({ userMessage: event.target.value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    if (!this.state.userMessage.trim()) return;
+
+    const msg = {
+      text: this.state.userMessage,
+      user: 'human',
+    };
+
+    this.setState({
+      conversation: [...this.state.conversation, msg],
+    });
+
+    fetch('http://localhost:5000/chat', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        message: this.state.userMessage,
+      }),
+    });
+
+    this.setState({ userMessage: '' });
+  };
+
   render() {
+    const ChatBubble = (text, i, className) => {
+      return (
+        <div key={`${className}-${i}`} className={`${className} chat-bubble`}>
+          <span className="chat-content">{text}</span>
+        </div>
+      );
+    };
+
+    const chat = this.state.conversation.map((e, index) =>
+      ChatBubble(e.text, index, e.user)
+    );
+
     return (
-      <div className="App">
-        <NavBar/>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h1>React Chatbot</h1>
+        <div className="chat-window">
+          <div className="conversation-view">{chat}</div>
+          <div className="message-box">
+            <form onSubmit={this.handleSubmit}>
+              <input
+                value={this.state.userMessage}
+                onInput={this.handleChange}
+                className="text-input"
+                type="text"
+                autoFocus
+                placeholder="Type your message and hit Enter to send"
+              />
+            </form>
+          </div>
+        </div>
       </div>
     );
   }
 }
 
 export default App;
+
+
+// import React, { Component } from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+// import NavBar from './components';
+//
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <NavBar/>
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <p>
+//             Edit <code>src/App.js</code> and save to reload.
+//           </p>
+//           <a
+//             className="App-link"
+//             href="https://reactjs.org"
+//             target="_blank"
+//             rel="noopener noreferrer"
+//           >
+//             Learn React
+//           </a>
+//         </header>
+//       </div>
+//     );
+//   }
+// }
+//
+// export default App;

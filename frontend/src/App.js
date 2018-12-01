07d6b7d362e79a7792c33a46e65bffbabc5419bf
Peggy Li <xpeggyxli@gmail.com>
2018-12-01 13:07:43 -0500

    Add navBar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Chat } from './views';
+import NavBar from './components';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <Router>
-        <Route exact path="/" component={Chat} />
+        <div>
+          <NavBar/>
+          <Route exact path="/" component={Chat} />
+        </div>
       </Router>
     );
   }

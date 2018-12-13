a5c7b5c6e7196a97cf7e9b8503a25c90bfab8f71
Peggy Li <xpeggyxli@gmail.com>
2018-12-13 15:19:58 -0500

    fix navbar

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -38,9 +38,6 @@ function NavBar(props) {
           <Typography variant="h6" color="inherit" className={classes.grow}>
             Kaiya
           </Typography>
-          <Link to="/login" className={classes.link}>
-            <Button color="inherit">Login</Button>
-          </Link>
           <Link to="/journal" className={classes.link}>
             <Button color="inherit">Journal</Button>
           </Link>
